e5cf35be846fcf277c8f127bf84d3f6d5daa61a1
Gabriel Monteiro <57967676+gabgmont@users.noreply.github.com>
2020-10-31 11:32:33 -0300

    Teste de leitura do arquivo
    Realizado a leitura do arquivo .txt contendo o Log da corrida.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,23 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String local = "C:\\Users\\gabgm\\OneDrive\\Documentos\\Gabriel\\arquivos\\log.txt";
+
+        try(BufferedReader leitor = new BufferedReader (new FileReader(local))){
+            String linha = leitor.readLine();
+            while(leitor.readLine() != null){
+                System.out.println(linha);
+                linha = leitor.readLine();
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
